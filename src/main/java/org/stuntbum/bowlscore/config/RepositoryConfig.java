497960f3cd9f419371f27edea80b33e63848a8bf
Mikko Lundgren <mikko@Mikkos-MacBook-Pro.local>
2016-01-03 18:51:56 +0200

    added database_url

--- src/main/java/org/stuntbum/bowlscore/config/RepositoryConfig.java
+++ src/main/java/org/stuntbum/bowlscore/config/RepositoryConfig.java
@@ -1,9 +1,15 @@
 package org.stuntbum.bowlscore.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by mikko on 03/01/16.
@@ -13,4 +19,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @EntityScan(basePackages = "org.stuntbum.bowlscore.domain")
 public class RepositoryConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        //String username = dbUri.getUserInfo().split(":")[0];
+        //String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        //basicDataSource.setUsername(username);
+        //basicDataSource.setPassword(password);
+        return basicDataSource;
+    }
 }
